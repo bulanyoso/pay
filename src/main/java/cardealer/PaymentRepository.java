ee24b3ce05d0f3e89f9b7e384aa9347c980f6dfc
bulanyoso <cchcc3@gmail.com>
2021-02-04 04:27:00 +0900

    v1

---
+++ src/main/java/cardealer/PaymentRepository.java
@@ -0,0 +1,8 @@
+package cardealer;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface PaymentRepository extends PagingAndSortingRepository<Payment, Long>{
+
+
+}
